abb250629a702650f2ca941c40aa1742b72cc7e5
myscreencode18 <147525475+myscreencode18@users.noreply.github.com>
2025-12-14 16:06:44 +0530

    Fix navbar links with React Router

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { Menu } from "lucide-react";
 import { useLocation } from "react-router-dom"; 
+import { Link } from "react-router-dom";
 export default function Navbar() {
   const [open, setOpen] = useState(false);
 const location = useLocation();
@@ -32,18 +33,19 @@ const isDarkPage = darkPages.includes(location.pathname);
               className="absolute top-0 right-10 mt-2 flex flex-col space-y-4 text-lg bg-black/80 backdrop-blur-sm px-6 py-4 rounded-lg"
               style={{ fontFamily: "var(--font-heading)" }}
             >
-              {["Work", "About", "Services", "Stories","Contact"].map((item, i) => (
-                <motion.a
-                  key={i}
-                  href={`${item.toLowerCase()}`}
-                  initial={{ opacity: 0, x: 20 }}
-                  animate={{ opacity: 1, x: 0 }}
-                  transition={{ delay: i * 0.1 }}
-                  className="text-white hover:text-[var(--color-accent)] transition"
-                >
-                  {item}
-                </motion.a>
-              ))}
+             {["Work", "About", "Services", "Stories","Contact"].map((item, i) => (
+  <motion.div
+    key={i}
+    initial={{ opacity: 0, x: 20 }}
+    animate={{ opacity: 1, x: 0 }}
+    transition={{ delay: i * 0.1 }}
+    className="text-white hover:text-[var(--color-accent)] transition"
+  >
+    <Link to={`/${item.toLowerCase()}`}>
+      {item}
+    </Link>
+  </motion.div>
+))}
             </motion.nav>
           )}
         </AnimatePresence>
